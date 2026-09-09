c9a463ffed7bf7c5adc346977ca4a6489560adc0
Zumra <s2401127@edu.bc.fi>
2026-09-09 10:47:25 +0300

    Olion tietojen käyttäminen komponentissa -t2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import Tervehdys from "./components/Tervehdys";
 
+function OpiskelijaTiedot() {
+  const opiskelija = {
+    nimi: "Matti Meikäläinen",
+    ika: 16,
+    kurssi: "Reactin perusteet",
+  };
+
+  return (
+    <div>
+      <h2>Opiskelijan tiedot</h2>
+      <p>Nimi: {opiskelija.nimi}</p>
+      <p>Ikä: {opiskelija.ika}</p>
+      <p>Kurssi: {opiskelija.kurssi}</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <Tervehdys nimi="Mika" />
+
+      <OpiskelijaTiedot />
     </div>
   );
 }
